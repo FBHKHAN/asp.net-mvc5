eb2e9040a4104e2b070e88211695b779fe8aad3e
Farooq Khan <fbhkhan92@gmail.com>
2016-07-18 23:41:35 -0400

    Initial Commit

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie()
+            {
+                Name = "Shrek!", 
+                Id = 456
+            };
+
+            return View(movie);
+            //return Content("Hello Shrekky");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            //return RedirectToAction("Index", "Home", new {page = 1, sortBy = "name"});
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+    }
+}
